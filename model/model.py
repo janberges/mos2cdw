b852cbd3858e17a640239a30ca3d8cb7dcd2b32d
Jan Berges <jan.berges@uni-bremen.de>
2024-02-08 16:26:30 +0100

    Fit model coupling to Nina's DFPT data

---
+++ model/model.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import elphmod
+
+def setup_coupling(
+    t_z2=-0.14,
+    t_z2_x2y2=0.48,
+    t_z2_xy=0.38,
+    t_x2y2=-0.26,
+    t_x2y2_xy=-0.31,
+    t_xy=0.32,
+
+    a=3.185,
+
+    M=95.95,
+    m=32.06,
+
+    beta=5.0):
+
+    t0 = np.array([
+        [ t_z2,      t_z2_x2y2, t_z2_xy  ],
+        [ t_z2_x2y2, t_x2y2,    t_x2y2_xy],
+        [-t_z2_xy,  -t_x2y2_xy, t_xy     ],
+        ]) / elphmod.misc.Ry
+
+    a = a / elphmod.misc.a0
+
+    def R(phi):
+        return np.array([
+            [1,               0,                0],
+            [0, np.cos(2 * phi), -np.sin(2 * phi)],
+            [0, np.sin(2 * phi),  np.cos(2 * phi)],
+            ])
+
+    def dR_dphi(phi):
+        return 2 * np.array([
+            [0,                0,                0],
+            [0, -np.sin(2 * phi), -np.cos(2 * phi)],
+            [0,  np.cos(2 * phi), -np.sin(2 * phi)],
+            ])
+
+    def derivative(t0, phi):
+        dt_dr = -beta / a * np.einsum('ab,bc,cd->ad', R(phi), t0, R(-phi))
+
+        dt_dphi = (
+              np.einsum('ab,bc,cd->ad', dR_dphi(phi), t0, R(-phi))
+            - np.einsum('ab,bc,cd->ad', R(phi), t0, dR_dphi(-phi))
+            )
+
+        dt_dx = dt_dr * np.cos(phi) - dt_dphi / a * np.sin(phi)
+        dt_dy = dt_dr * np.sin(phi) + dt_dphi / a * np.cos(phi)
+
+        return np.array([dt_dx, dt_dy])
+
+    dt = np.zeros((6, 9, 3, 3))
+
+    for n in range(6):
+        dt[n, :2] = derivative(t0.T if n % 2 else t0,
+            n * 60 * elphmod.bravais.deg)
+
+    sqrtM = np.sqrt(np.repeat([M, m, m], 3)[:, np.newaxis, np.newaxis]
+        * elphmod.misc.uRy)
+
+    def coupling(q1=0, q2=0, q3=0, k1=0, k2=0, k3=0, **ignore):
+        """Calculate coupling according to Eq. (B4) of arXiv:1911.02450."""
+
+        K1 = k1 + q1
+        K2 = k2 + q2
+
+        return (
+            + dt[0] * (np.exp(1j * k1) - np.exp(1j * K1))
+            + dt[3] * (np.exp(-1j * k1) - np.exp(-1j * K1))
+            + dt[1] * (np.exp(1j * (k1 + k2)) - np.exp(1j * (K1 + K2)))
+            + dt[4] * (np.exp(-1j * (k1 + k2)) - np.exp(-1j * (K1 + K2)))
+            + dt[2] * (np.exp(1j * k2) - np.exp(1j * K2))
+            + dt[5] * (np.exp(-1j * k2) - np.exp(-1j * K2))
+            ) / sqrtM
+
+    return coupling
+
+def save_coupling(stem, coupling, el, ph):
+    nq = nk = (3, 3, 1)
+
+    q = [(q1, q2, q3)
+        for q1 in range(nq[0])
+        for q2 in range(nq[1])
+        for q3 in range(nq[2])]
+
+    q = 2 * np.pi * np.array(q, dtype=float) / nq
+
+    g = elphmod.elph.sample(coupling, q.reshape((-1, 3)), nk)
+
+    Rk, dk, lk = elphmod.bravais.wigner_seitz_x('k', nk[0], ph.a)
+    Rg, dg, lg = elphmod.bravais.wigner_seitz_x('g', nq[0], ph.a,
+        ph.r - ph.r[0])
+
+    Rk = np.insert(Rk, obj=2, values=0, axis=1)
+    Rg = np.insert(Rg, obj=2, values=0, axis=1)
+
+    dk = np.reshape(dk, (1, 1, len(Rk)))
+    dg = np.reshape(dg, (1, 1, 3, len(Rg)))
+
+    elph = elphmod.elph.Model(Rk=Rk, dk=dk, Rg=Rg, dg=dg, el=el, ph=ph,
+        divide_mass=False)
+    elphmod.elph.q2r(elph, nq, nk, g)
+    elph.standardize(eps=1e-10)
+
+    if elphmod.MPI.comm.rank == 0:
+        with open('%s.wigner' % stem, 'wb') as data:
+            for obj in [1, 1,
+                    len(elph.Rk), elph.Rk, np.ones(len(elph.Rk), dtype=int),
+                    len(elph.Rg), elph.Rg, np.ones(len(elph.Rg), dtype=int)]:
+                np.array(obj, dtype=np.int32).tofile(data)
+
+        with open('%s.epmatwp' % stem, 'wb') as data:
+            np.swapaxes(elph.data, 3, 4).astype(np.complex128).tofile(data)
---
+++ model/optimize.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import elphmod
+import model
+import numpy as np
+import scipy.optimize
+import storylines
+
+comm = elphmod.MPI.comm
+
+nk = 48
+
+AFMhot = storylines.colormap( # Gnuplot
+    (0.00, storylines.Color(0, 0, 0)),
+    (0.25, storylines.Color(128, 0, 0)),
+    (0.50, storylines.Color(255, 128, 0)),
+    (0.75, storylines.Color(255, 255, 128)),
+    (1.00, storylines.Color(255, 255, 255)),
+    )
+
+q = np.array([[0.0, np.pi, 0.0]])
+
+BZ = dict(points=100, outside=np.nan)
+
+el = elphmod.el.Model('../dft/mos2')
+ph = elphmod.ph.Model('../dft/mos2.ifc', apply_asr_simple=True)
+elph = elphmod.elph.Model('../dft/mos2.epmatwp', '../dft/mos2.wigner', el, ph)
+
+w2, u = elphmod.dispersion.dispersion(ph.D, q, vectors=True)
+
+g0 = elph.sample(q, (nk, nk), u=u[:, :, 0:1])
+g02 = abs(g0) ** 2
+
+def error(t):
+    coupling = model.setup_coupling(*t)
+
+    g = elphmod.elph.sample(coupling, q, (nk, nk), u=u[:, :, 0:1])
+
+    error = (abs(g - g0) ** 2).sum()
+
+    print(('%9.5f' * 7) % (error, *t))
+
+    return error
+
+t = scipy.optimize.minimize(error, [-0.14, 0.48, -0.38, -0.26, 0.31, 0.32]).x
+
+coupling = model.setup_coupling(*t)
+
+model.save_coupling('model', coupling, el, ph)
+
+g = elphmod.elph.sample(coupling, q, (nk, nk), u=u[:, :, 0:1])
+g2 = abs(g) ** 2
+
+g2max = max(g02.max(), g2.max())
+
+for name, coupling in ('coupling_mod', g2), ('coupling_ref', g02):
+    for a in range(el.size):
+        for b in range(el.size):
+            data = elphmod.plot.toBZ(coupling[0, 0, :, :, a, b], **BZ)
+
+            image = storylines.colorize(data, cmap=AFMhot,
+                minimum=0, maximum=g2max)
+
+            if comm.rank == 0:
+                plot = storylines.Plot(background='%s%d%d.png' % (name, a, b),
+                    width=1.0, height=0, xyaxes=False, margin=0.05)
+
+                plot.line(*zip(*elphmod.bravais.BZ()), thick=True)
+
+                storylines.save(plot.background, image)
+
+                plot.save('%s%d%d.pdf' % (name, a, b))
+
+    if comm.rank == 0:
+        storylines.combine('%s.pdf' % name, ['%s%d%d' % (name, a, b)
+            for a in range(el.size) for b in range(el.size)], columns=el.size)
