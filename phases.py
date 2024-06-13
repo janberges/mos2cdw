2e9002efffaee375cfd8b63c8afc988a6ef7c54f
Jan Berges <jan.berges@uni-bremen.de>
2024-06-13 13:17:46 +0200

    Relax and compute λ on same q and k meshes

--- phases.py
+++ phases.py
@@ -7,11 +7,6 @@ import scipy.optimize
 comm = elphmod.MPI.comm
 info = elphmod.MPI.info
 
-nk = nq = 12
-kTel = 0.01
-f = elphmod.occupations.fermi_dirac
-eps = 1e-10
-
 dopings = np.linspace(0.0, 0.6, 61)
 
 pw = elphmod.bravais.read_pwi('dft/MoS2.pwi')
@@ -33,23 +28,6 @@ driver = elphmod.md.Driver(elph,
 
 driver.kT = 0.005
 driver.f = elphmod.occupations.fermi_dirac
-
-q = sorted(elphmod.bravais.irreducibles(nq))
-q = 2 * np.pi / nq * np.array(q, dtype=float)
-
-g = driver.elph.sample(q, nk=nk, shared_memory=True)
-
-node, images = elphmod.MPI.shm_split()
-
-if comm.rank == 0:
-    g /= np.repeat(np.sqrt(driver.elph.ph.M), 3)[np.newaxis, :,
-        np.newaxis, np.newaxis, np.newaxis, np.newaxis]
-    g *= elphmod.misc.Ry ** 1.5
-
-if node.rank == 0:
-    images.Bcast(g)
-
-kTel *= elphmod.misc.Ry
 
 driver.plot(scale=10.0, interactive=True)
 
@@ -74,26 +52,16 @@ for doping in dopings:
 
     driver.diagonalize()
 
-    ph = driver.phonons(apply_asr_simple=True)
+    lamda, wlog, w2nd = driver.superconductivity(kT=0.01)
 
-    w2, u = elphmod.dispersion.dispersion(ph.D, q, vectors=True)
-    w2 *= elphmod.misc.Ry ** 2
-
-    if np.any(w2 < -1e-8):
+    if lamda is None:
         info('Imaginary frequencies!')
         continue
 
-    el = driver.electrons()
+    wlog *= elphmod.misc.Ry
+    w2nd *= elphmod.misc.Ry
 
-    e, U = elphmod.dispersion.dispersion_full_nosym(el.H, nk, vectors=True)
-    e = e[..., el.size // 3:]
-    U = U[..., el.size // 3:]
-
-    g2 = elphmod.elph.transform(g, q, nk, U, u, squared=True,
-        shared_memory=True)
-
-    lamda, wlog, Tc, w2nd = elphmod.eliashberg.McMillan(nq, e, w2, g2, eps,
-        mustar=0.0, kT=kTel, f=f, correct=True)
+    Tc = elphmod.eliashberg.Tc(lamda, wlog, 0.0, w2nd, correct=True)
 
     if comm.rank == 0:
         data.write(' %9.6f' * 6 % (doping, lamda, wlog, w2nd, Tc,
