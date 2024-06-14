9b4ec93df92161124e5a2d40f76f7f45c6f0cad3
Jan Berges <jan.berges@uni-bremen.de>
2024-06-14 10:13:57 +0200

    Compute critical temperatures for large supercell

--- relax_large.py
+++ relax_large.py
@@ -3,8 +3,12 @@
 import elphmod
 import numpy as np
 import scipy.optimize
+import sys
 
+comm = elphmod.MPI.comm
 info = elphmod.MPI.info
+
+dopings = np.linspace(0.26, 0.31, 6)
 
 pw = elphmod.bravais.read_pwi('dft/MoS2.pwi')
 
@@ -50,3 +54,46 @@ scipy.optimize.minimize(driver.free_energy, driver.u, jac=driver.jacobian,
 driver.plot(interactive=False)
 
 driver.to_xyz('relax_large.xyz')
+
+if len(sys.argv) > 1:
+    idoping = int(sys.argv[1])
+    dopings = dopings[idoping - 1:idoping]
+    filename = 'relax_large_%d.dat' % idoping
+else:
+    filename = 'relax_large.dat'
+
+if comm.rank == 0:
+    data = open(filename, 'w')
+    data.write(' %9s' * 6
+        % ('doping/e', 'lamda', 'wlog/eV', 'w2nd/eV', 'Tc/K', '|u|/AA'))
+    data.write('\n')
+
+for doping in dopings:
+    info('Setting the doping to %g electrons per unit cell...' % doping)
+
+    driver.n = (2 + doping) * len(driver.elph.cells)
+
+    driver.from_xyz('relax_large.xyz')
+
+    scipy.optimize.minimize(driver.free_energy, driver.u, jac=driver.jacobian,
+        method='BFGS', options=dict(gtol=1e-6, norm=np.inf))
+
+    driver.diagonalize()
+
+    lamda, wlog, w2nd = driver.superconductivity(kT=0.01)
+
+    wlog *= elphmod.misc.Ry
+    w2nd *= elphmod.misc.Ry
+
+    Tc = elphmod.eliashberg.Tc(lamda, wlog, 0.0, w2nd, correct=True)
+
+    if comm.rank == 0:
+        data.write(' %9.6f' * 6 % (doping, lamda, wlog, w2nd, Tc,
+            np.linalg.norm(driver.u) * elphmod.misc.a0
+                * 4 / len(driver.elph.cells)))
+        data.write('\n')
+
+    info('The critical temperature is %g K.' % Tc)
+
+if comm.rank == 0:
+    data.close()
---
+++ relax_large.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --nodes 1
+#SBATCH --tasks-per-node 96
+#SBATCH --partition standard96:test
+#SBATCH --time 01:00:00
+
+module load anaconda3 intel impi
+
+cd $SLURM_SUBMIT_DIR
+
+export SLURM_CPU_BIND=none
+export MKL_NUM_THREADS=16
+export NUMEXPR_NUM_THREADS=16
+export OMP_NUM_THREADS=16
+export OPENBLAS_NUM_THREADS=16
+export VECLIB_MAXIMUM_THREADS=16
+
+for i in `seq 1 6`
+do
+    echo "Starting calculation for doping $i.."
+    python3 relax_large.py $i > relax_large_$i.out &
+done
+
+wait
