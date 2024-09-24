7d4fca632a34da7e5db8a09a15b69ff8a615e1cb
Jan Berges <jan.berges@uni-bremen.de>
2024-09-24 11:57:47 +0200

    Start calculations for whole phase diagram

--- paper/18sqrt3.py
+++ paper/18sqrt3.py
@@ -25,13 +25,16 @@ driver = elphmod.md.Driver(elph,
     supercell=[(36, 18, 0), (-18, 18, 0)],
 )
 
-mu0 = driver.mu
+cells = len(driver.elph.cells)
 
 driver.kT = 0.005
 driver.f = elphmod.occupations.fermi_dirac
 
-def triangle(atoms=[1471, 1474, 1555], amplitude=0.2):
+def symmetric():
     driver.u[:] = 0.0
+
+def triangle(atoms=[1471, 1474, 1555], amplitude=0.2):
+    symmetric()
 
     center = np.average(driver.elph.ph.r[atoms], axis=0)
 
@@ -45,9 +48,9 @@ def triangles(distortion=0.02, amplitude=0.1):
     driver.random_displacements(distortion)
     random = driver.u.copy()
 
-    driver.u[:] = 0.0
+    symmetric()
 
-    dim = 3 * driver.elph.ph.nat // len(driver.elph.cells)
+    dim = 3 * driver.elph.ph.nat // cells
     u = amplitude * np.array([1.0, 0.0])
 
     for n, (i, j, k) in enumerate(driver.elph.cells):
@@ -62,30 +65,32 @@ def optimize():
     scipy.optimize.minimize(driver.free_energy, driver.u, jac=driver.jacobian,
         method='BFGS', options=dict(gtol=1e-5, norm=np.inf))
 
-rank = int(sys.argv[1]) if len(sys.argv) > 1 else 0
-size = int(sys.argv[2]) if len(sys.argv) > 2 else 1
+driver.diagonalize()
 
-if comm.rank == 0:
-    data = open('polaron%d.dat' % rank, 'w')
-    data.write(' %9s' * 7
-        % ('nel', 'mu/eV', 'lamda', 'wlog/eV', 'w2nd/eV', 'Tc/K', '|u|/AA'))
-    data.write('\n')
-    data.flush()
+mu0 = driver.mu
 
-for nel in range(243 + rank, 343, size):
-    driver.n = 2 * len(driver.elph.cells) + nel
+start = int(sys.argv[1]) if len(sys.argv) > 1 else 0
+stop = int(sys.argv[2]) if len(sys.argv) > 2 else start + 1
+step = int(sys.argv[3]) if len(sys.argv) > 3 else 1
 
+for nel in range(start, stop, step):
     info('Number of doping electrons: %g' % nel)
+
+    driver.n = 2 * cells + nel
+
+    symmetric()
+
+    E0 = driver.free_energy()
 
     triangle()
     optimize()
-    driver.to_xyz('polaron%d.xyz' % nel)
+    driver.to_xyz('polaron%03d.xyz' % nel)
 
     #triangles()
     #optimize()
-    #driver.to_xyz('cdw%d.xyz' % nel)
+    #driver.to_xyz('cdw%03d.xyz' % nel)
 
-    driver.diagonalize()
+    E = driver.free_energy()
 
     lamda, wlog, w2nd = driver.superconductivity()
 
@@ -94,14 +99,17 @@ for nel in range(243 + rank, 343, size):
 
     Tc = elphmod.eliashberg.Tc(lamda, wlog, 0.0, w2nd, correct=True)
 
-    if comm.rank == 0:
-        data.write(' %9.6f' * 7 % (nel, driver.mu - mu0, lamda, wlog, w2nd, Tc,
-            np.linalg.norm(driver.u) * elphmod.misc.a0
-                / np.sqrt(len(driver.elph.cells))))
-        data.write('\n')
-        data.flush()
-
     info('The critical temperature is %g K.' % Tc)
 
-if comm.rank == 0:
-    data.close()
+    if comm.rank != 0:
+        continue
+
+    with open('polaron%03d.dat' % nel, 'w') as data:
+        data.write(('%3s' + ' %9s' * 8 + '\n') % ('nel', 'xel',
+            'dE/eV', 'mu/eV', '|u|/AA', 'lamda', 'wlog/eV', 'w2nd/eV', 'Tc/K'))
+
+        data.write(('%3d' + ' %9.6f' * 8 + '\n') % (nel, nel / cells,
+            (E - E0) * elphmod.misc.Ry / cells,
+            (driver.driver.mu - mu0) * elphmod.misc.Ry,
+            np.linalg.norm(driver.u) * elphmod.misc.a0 / np.sqrt(cells)),
+            lamda, wlog, w2nd, Tc)
--- paper/18sqrt3.sh
+++ paper/18sqrt3.sh
@@ -12,15 +12,21 @@ module load miniconda3
 cd $SLURM_SUBMIT_DIR
 
 export SLURM_CPU_BIND=none
-export MKL_NUM_THREADS=16
-export NUMEXPR_NUM_THREADS=16
-export OMP_NUM_THREADS=16
-export OPENBLAS_NUM_THREADS=16
-export VECLIB_MAXIMUM_THREADS=16
+export MKL_NUM_THREADS=6
+export NUMEXPR_NUM_THREADS=6
+export OMP_NUM_THREADS=6
+export OPENBLAS_NUM_THREADS=6
+export VECLIB_MAXIMUM_THREADS=6
 
-for rank in `seq 0 5`
+#for nel in `seq -w 0 6 186`
+#for nel in `seq 192 6 240` `seq 342 6 474`
+#for nel in `seq 288 6 474`
+#for nel in `seq 480 6 666`
+for nel in `seq 243 274`
+#for nel in `seq 275 306`
+#for nel in `seq 307 338`
 do
-    python3 18sqrt3.py $rank 6 > 18sqrt3_$rank.out &
+    python3 18sqrt3.py $nel > 18sqrt3_$nel.out &
 done
 
 wait
