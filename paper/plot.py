603073bbb158e77b1a94b2f92eb7e909fa8e663e
Jan Berges <jan.berges@uni-bremen.de>
2024-11-12 09:08:10 +0100

    Rename plot scripts

--- paper/plot.py
+++ paper/plot.py
@@ -1,44 +1,84 @@
 #!/usr/bin/env python3
 
+import elphmod
 import numpy as np
 import storylines
-import matplotlib.pyplot as plt
+
+nel1, xel1, dE1, mu1, u1, lamda1, wlog1, w2nd1, wmin, Tc1 = np.loadtxt('polaron_new.dat',
+    skiprows=1).T
+
+nel2, xel2, dE2, mu2, u2, lamda2, wlog2, w2nd2, wmin, Tc2 = np.loadtxt('cdw_new.dat',
+    skiprows=1).T
+
+pol = u1 > 2e-3
+sym = ~pol
+cdw = u2 > 2e-3
+
+(nel0, xel0, dE0, mu0, u0, lamda0, wlog0, w2nd0, Tc0) = (nel1[sym], xel1[sym],
+    dE1[sym], mu1[sym], u1[sym], lamda1[sym], wlog1[sym], w2nd1[sym], Tc1[sym])
+
+(nel1, xel1, dE1, mu1, u1, lamda1, wlog1, w2nd1, Tc1) = (nel1[pol], xel1[pol],
+    dE1[pol], mu1[pol], u1[pol], lamda1[pol], wlog1[pol], w2nd1[pol], Tc1[pol])
+
+(nel2, xel2, dE2, mu2, u2, lamda2, wlog2, w2nd2, Tc2) = (nel2[cdw], xel2[cdw],
+    dE2[cdw], mu2[cdw], u2[cdw], lamda2[cdw], wlog2[cdw], w2nd2[cdw], Tc2[cdw])
 
 plot = storylines.Plot(
     style='APS',
     font='Utopia',
 
-    xlabel=r'Doping electrons per unit cell',
+    width=9.0,
+    height=7.0,
+
+    xlabel='Doping electrons per unit cell',
+    ylabel='Critical temperature (K)',
 
     xstep=0.1,
-    ystep=5.0,
 
     grid=True,
 
-    lpos='tl',
-    lopt='below right=1mm',
-    lbox=True,
-    )
+    lpos='cb',
+    lopt='above=2mm',
+    lbox=True)
 
-plot.height = plot.width
+plot.line(xel0, Tc0, color='blue', label='undistorted')
 
-for file in 'polaron', 'cdw':
-    nel, xel, dE, mu, u, lamda, wlog, w2nd, wmin, Tc = np.loadtxt('%s_new.dat' % file,
-        skiprows=1).T
+for group in elphmod.misc.group(nel1, 1.1):
+    plot.line(xel1[group], Tc1[group], color='brown',
+        mark='asterisk' if len(group) == 1 else None,
+        label='polaron' if len(group) == 1 else None)
 
-    if file == 'polaron':
-        mu0 = mu[0]
+plot.line(xel2, Tc2, color='orange', label=r'$2 \times 2$ CDW')
 
-    for fun, x in (plot.line, xel), (plt.plot, nel):
-        fun(x, -dE * 1e3, color='orange', label=r'$-\Delta E$ (meV)')
-        fun(x, 10 * (mu - mu0), color='yellow', label=r'$10 \mu$ (eV)')
-        fun(x, u * 1e2, color='cyan', label=r'$|u|_{2 \times 2}$ (pm)')
-        fun(x, lamda, color='red', label=r'$\lambda$')
-        fun(x, wlog * 1e3, color='gray', label=r'$\omega_{\mathrm{log}}$ (meV)')
-        fun(x, w2nd * 1e3, color='lightgray', label=r'$\omega_{\mathrm{2nd}}$ (meV)')
-        fun(x, Tc, color='blue', label=r'$T_{\mathrm{c}}$ (K)')
+plot.save('tc_18sqrt3.pdf')
 
-plt.legend()
-plt.show()
+plot.width = 5.0
+plot.height = 2.5
+plot.lput = False
 
-plot.save('plot.pdf')
+plot.xlabel = '$x$'
+plot.ylabel = r'$T_{\mathrm c}$ (K)'
+
+plot.xclose = True
+plot.yclose = True
+
+plot.xticks = [0.0, (0.1, None), (0.2, None), (0.3, None), (0.4, None), (0.5, None), 0.6]
+plot.yticks = [0.0, (10.0, None), (20.0, None), 30.0]
+
+plot.left = 0.5
+plot.bottom = 0.5
+
+for label, color, nel, xel, Tc in [
+    ('polaron', 'blue', nel0, xel0, Tc0),
+    ('polaron', 'brown', nel1, xel1, Tc1),
+    ('cdw', 'orange', nel2, xel2, Tc2),
+    ]:
+
+    for i in range(len(nel)):
+        print('%s%03d' % (label, nel[i]))
+
+        plot.line(xel[i], Tc[i], mark='*', color=color)
+
+        plot.save('png/%s%03dtc.png' % (label, nel[i]))
+
+        plot.lines.pop()
---
+++ paper/plot_si.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import storylines
+import matplotlib.pyplot as plt
+
+plot = storylines.Plot(
+    style='APS',
+    font='Utopia',
+
+    xlabel=r'Doping electrons per unit cell',
+
+    xstep=0.1,
+    ystep=5.0,
+
+    grid=True,
+
+    lpos='tl',
+    lopt='below right=1mm',
+    lbox=True,
+    )
+
+plot.height = plot.width
+
+for file in 'polaron', 'cdw':
+    nel, xel, dE, mu, u, lamda, wlog, w2nd, wmin, Tc = np.loadtxt('%s_new.dat' % file,
+        skiprows=1).T
+
+    if file == 'polaron':
+        mu0 = mu[0]
+
+    for fun, x in (plot.line, xel), (plt.plot, nel):
+        fun(x, -dE * 1e3, color='orange', label=r'$-\Delta E$ (meV)')
+        fun(x, 10 * (mu - mu0), color='yellow', label=r'$10 \mu$ (eV)')
+        fun(x, u * 1e2, color='cyan', label=r'$|u|_{2 \times 2}$ (pm)')
+        fun(x, lamda, color='red', label=r'$\lambda$')
+        fun(x, wlog * 1e3, color='gray', label=r'$\omega_{\mathrm{log}}$ (meV)')
+        fun(x, w2nd * 1e3, color='lightgray', label=r'$\omega_{\mathrm{2nd}}$ (meV)')
+        fun(x, Tc, color='blue', label=r'$T_{\mathrm{c}}$ (K)')
+
+plt.legend()
+plt.show()
+
+plot.save('plot.pdf')
