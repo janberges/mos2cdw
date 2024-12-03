570871ef6de5ee452c7063641bf69a348d2a1024
Jan Berges <jan.berges@uni-bremen.de>
2024-12-03 08:50:48 +0100

    Plot model and reference coupling with Matplotlib

--- model_plot.py
+++ model_plot.py
@@ -1,24 +1,11 @@
 #!/usr/bin/env python3
 
 import elphmod
+import matplotlib.pyplot as plt
 import numpy as np
-import storylines
-
-comm = elphmod.MPI.comm
 
 nk = 48
-
-AFMhot = storylines.colormap( # Gnuplot
-    (0.00, storylines.Color(0, 0, 0)),
-    (0.25, storylines.Color(128, 0, 0)),
-    (0.50, storylines.Color(255, 128, 0)),
-    (0.75, storylines.Color(255, 255, 128)),
-    (1.00, storylines.Color(255, 255, 255)),
-    )
-
 q = np.array([[0.0, np.pi]])
-
-BZ = dict(points=100, outside=np.nan)
 
 el = elphmod.el.Model('data/MoS2_3')
 ph = elphmod.ph.Model('data/MoS2.ifc', apply_asr_simple=True)
@@ -27,29 +14,28 @@ model = elphmod.elph.Model('data/model.epmatwp', 'data/model.wigner', el, ph)
 
 w2, u = elphmod.dispersion.dispersion(ph.D, q, vectors=True)
 
-g02 = elph.sample(q, (nk, nk), u=u[..., 1:2], squared=True)
-g2 = model.sample(q, (nk, nk), u=u[..., 1:2], squared=True)
+g2 = np.empty((2, 1, 1, nk, nk, el.size, el.size))
 
-g2max = max(g02.max(), g2.max())
+g2[0] = model.sample(q, (nk, nk), u=u[..., 1:2], squared=True)
+g2[1] = elph.sample(q, (nk, nk), u=u[..., 1:2], squared=True)
 
-for name, coupling in ('model_plot_mod', g2), ('model_plot_ref', g02):
+figure, axes = plt.subplots(3, 6, figsize=(16, 8))
+
+orb = ['d_{z^2}', 'd_{x^2 - y^2}', 'd_{x y}']
+src = ['mod.', 'ref.']
+
+for i in range(g2.shape[0]):
     for a in range(el.size):
         for b in range(el.size):
-            data = elphmod.plot.toBZ(coupling[0, 0, :, :, a, b], **BZ)
+            image = elphmod.plot.toBZ(g2[i, 0, 0, :, :, a, b], points=100,
+                outside=np.nan)
 
-            image = storylines.colorize(data, cmap=AFMhot,
-                minimum=0, maximum=g2max)
+            if elphmod.MPI.comm.rank == 0:
+                ax = axes[a, 2 * b + i]
 
-            if comm.rank == 0:
-                plot = storylines.Plot(background='%s%d%d.png' % (name, a, b),
-                    width=1.0, height=0, xyaxes=False, margin=0.05)
+                ax.set_title(r'$%s, %s$ (%s)' % (orb[a], orb[b], src[i]))
+                ax.imshow(image, vmin=0, vmax=g2.max())
+                ax.axis('image')
+                ax.axis('off')
 
-                plot.line(*zip(*elphmod.bravais.BZ()), thick=True)
-
-                storylines.save(plot.background, image)
-
-                plot.save('%s%d%d.pdf' % (name, a, b))
-
-    if comm.rank == 0:
-        storylines.combine('%s.pdf' % name, ['%s%d%d' % (name, a, b)
-            for a in range(el.size) for b in range(el.size)], columns=el.size)
+plt.show()
